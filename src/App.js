864ed98cec886ef3b7f14a53a031431dfc509fde
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-03-02 17:31:26 +0100

    verspace proposal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useMemo } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { softShadows, MeshWobbleMaterial, OrbitControls } from "drei";
+import { useSpring, animated } from "react-spring/three";
+import * as THREE from "three/src/Three";
+import reactLogo from './Assets/reactLogo.jpg'
+import nodeLogo from './Assets/nodeLogo.png'
+import verspace from './Assets/verspace.png'
+import Olanrewaju from './Olanrewaju'
 
-function App() {
+softShadows();
+const Stars = () => {
+  let group = useRef();
+  let theta = 0;
+  useFrame(() => {
+    const r = 5 * Math.sin(THREE.Math.degToRad((theta += 0.1)));
+    const s = Math.cos(THREE.Math.degToRad(theta * 2));
+    group.current.rotation.set(r, r, r);
+    group.current.scale.set(s, s, s);
+  });
+  const [geo, mat, coords] = useMemo(() => {
+    const geo = new THREE.SphereBufferGeometry(1, 10, 10);
+    const mat = new THREE.MeshBasicMaterial({
+      color: new THREE.Color("lightblue"),
+    });
+    const coords = new Array(2000)
+      .fill()
+      .map((i) => [
+        Math.random() * 800 - 400,
+        Math.random() * 800 - 400,
+        Math.random() * 800 - 400,
+      ]);
+    return [geo, mat,  coords];
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <group ref={group}>
+      {coords.map(([p1, p2, p3], i) => (
+        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
+      ))}
+    </group>
   );
-}
+};
+const SpinningBox = ({ position, args, color, speed }) => {
+  const mesh = useRef(null);
+  const [expand, setExpand] = useState(false);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+  });
+  return (
+    <animated.mesh
+      onClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow
+      position={position}
+      ref={mesh}
+    >
+      <boxBufferGeometry attach="geometry" args={args} />
+
+      <MeshWobbleMaterial
+        attach="material"
+        map={color}
+        speed={speed}
+        factor={0.0}
+        
+      />
+    </animated.mesh>
+  );
+};
+
+const App = () => {
+  const reactlogo = new THREE.TextureLoader().load(reactLogo)
+  const nodelogo = new THREE.TextureLoader().load(nodeLogo)
+  const codng = new THREE.TextureLoader().load(verspace)
+  return (
+    <>
+    <div className="proposal_wrapper">
+      <Canvas shadowMap camera={{ position: [-5, 2, 10], fov: 60 }}>
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.4}
+          shadow-mapSize-height={1024}
+          shadow-mapSize-width={1024}
+          shadow-camera-far={50}
+          shadow-camera-right={10}
+          shadow-camera-left={-10}
+          shadow-camera-bottom={-10}
+          shadow-camera-top={10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.3} />
+        <pointLight position={[0, -10, 0]} intensity={1.3} />
+        <group>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+          <SpinningBox
+            position={[0, 1, 0]}
+            args={[3, 2, 1]}
+            color={codng}
+            
+            speed={2}
+          />
+          <SpinningBox position={[-2, 1, -5]} color={reactlogo} speed={4} />
+          <SpinningBox position={[5, 1, -2]} color={nodelogo} speed={4} />
+          <Stars/>
+        </group>
+        <OrbitControls/>
+      </Canvas>
+      <div className="proposal_text">
+      <Olanrewaju/>
+      </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/Olanrewaju.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Olanrewaju = () => {
+  return (
+    <>
+      <div className="heading">
+        <span>hi verspace. heard you in need of a </span>
+        <span>FrontEnd Developer</span>
+        <p>
+          well look no further i got you covered. my name is OLAYIWOLA
+          Olanrewaju and i am here to fill that position armed with React
+          and Nodejs,other technology in my armory include Redux,Tailwind,
+          Express,mongoDb,mySql and my tenacity at problem-solving,above all i
+          pride myself in ability to pick up new technology quickly. 
+          <a href="http://olanrewaju.herokuapp.com/">portfolio</a>
+          <a href="https://www.linkedin.com/in/olanrewaju-olayiwola/">linkedin</a>
+          <a href="https://github.com/larrycoal">github</a>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Olanrewaju;
